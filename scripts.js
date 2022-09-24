815393f6ec176c2298eb3e7773075a8486244816
LisaDavila <lisa.davila2244@gmail.com>
2022-09-24 06:45:39 -0500

    completed project

--- scripts.js
+++ scripts.js
@@ -4,11 +4,17 @@
 
 // 1. Read the code below one block at a time.
 // 2. Look for the @TODOs, and figure out how to fix them.
-    // next to each @TODO you will find tasks that need to be finished
+// next to each @TODO you will find tasks that need to be finished
 
 // The variable will change from X to O based on what player turn it is. We need to hold this so we can place an X or O on the board when they're clicked.
 let currentMarker = 'X'
+console.log("current marker is X")
 
+let board = [
+  ['', '', ''],
+  ['', '', ''],
+  ['', '', '']
+]
 
 
 
@@ -21,31 +27,25 @@ const handleClick = (element) => {
 
   // this next line prevents an X being changed to an O or an O being changed to an X by...
   //  checking to see if the square clicked has anything in it, if not continue
-  if(!document.getElementById(element.id).innerHTML){
+  if (!document.getElementById(element.id).innerHTML) {
     addMarker(element.id)
   }
 }
 
-
-
-
-
-
-
-
-
-
-
 // this function places the "currentMarker" inside the HTML element that was clicked and calls the "changeMarker" function.
 const addMarker = (id) => {
-  console.log("This is", id)
-
+let element = document.getElementById(id)
+const row = parseInt(element.id.charAt(0))
+const column = parseInt(element.id.charAt(2))
+  console.log("This is ", id)
+  board[row][column] = currentMarker
+  console.log("board",board)
   // @TODO-1: Open the console tab in your Chrome Inspector Tool and click on the top-left square to see what's logged to the console. 
   console.log(`*** The current marker is:  ${currentMarker}. ***`)
   console.log(`Therefore, a  "${currentMarker}"  should be placed in the square with the id:  ${id}`)
-  
+
   // @TODO-2: Build a line of code that will set the innerHTML property of the element that was clicked to the "currentMarker"
-  let element = document.getElementById(id)
+  
   console.log(element)
   element.innerHTML = currentMarker
   // @TODO-2.5: MIX & MATCH, You will need the following pieces of code to build that line:
@@ -54,24 +54,66 @@ const addMarker = (id) => {
   // document
   // .innerHTML 
 
-  changeMarker()
+
+  checkForWin()
 }
 
 
 
+//this is rows win
+const horizontalWin = () => {
+  if ( (board[0][0] == "X" && board[0][1] == "X" && board[0][2] == "X")
+    || (board[0][0] == "O" && board[0][1] == "O" && board[0][2] == "O")
+    || (board[0][1] == "X" && board[1][1] == "X" && board[1][2] == "X")
+    || (board[0][1] == "O" && board[1][1] == "O" && board[1][2] == "O")
+    || (board[0][2] == "X" && board[2][1] == "X" && board[2][2] == "X")
+    || (board[0][2] == "O" && board[2][1] == "O" && board[2][2] == "O")
+    )
+    
+    {console.log ("horizontalWin")
+    return true}
+  
+}
+
+//this is columns win
+const verticalWin = () => {
+  if ((board[0][0] == "X" && board[1][0] == "X" && board[2][0] == "X")
+    || (board[0][0] == "O" && board[1][0] == "O" && board[2][0] == "O")
+    || (board[0][1] == "X" && board[1][1] == "X" && board[2][1] == "X")
+    || (board[0][1] == "O" && board[1][1] == "O" && board[2][1] == "O")
+    || (board[0][2] == "X" && board[1][2] == "X" && board[2][2] == "X")
+    || (board[0][2] == "O" && board[1][2] == "O" && board[2][2] == "O"))
+    
+    {console.log ("Vertical Win")
+    return true}
+}
 
 
-
-
-
-
+  //this is diag win
+  const diagonalWin = () => {
+  if((board[0][0] == "X" && board[1][1] == "X" && board[2][2] == "X") 
+  || (board[0][0] == "O" && board[1][1] == "O" && board[2][2] == "O")
+  || (board[0][2] == "X" && board[1][1] == "X" && board[2][0] == "X") 
+  || (board[0][2] == "O" && board[1][1] == "O" && board[2][0] == "O")
+  )
+  {console.log ("Diagonal Win")
+    return true}
+}
 
 // This "changeMarker" function changes "X" to "O" in the "currentMarker" variable or "O" to "X"
 const changeMarker = () => {
-  if(currentMarker === "X"){
+  if (currentMarker === "X") {
     currentMarker = "O"
   } else {
     currentMarker = "X"
+  }
+}
+
+const checkForWin = () => {
+  if(horizontalWin() || verticalWin() || diagonalWin()) {
+    window.alert(`Player ${currentMarker} won!`)
+  } else {
+    changeMarker()
   }
 }
 
@@ -86,9 +128,9 @@ const changeMarker = () => {
 
 // This "resetBoard" function is called when the user clicks on the "Restart" button.
 const resetBoard = () => {
-  
+
   // @TODO-3: To make your "Restart" button work you'll need to build a line of code here that:
-      // collects all of the "td" elements into an HTML Collection: https://www.w3schools.com/jsref/dom_obj_htmlcollection.asp  
+  // collects all of the "td" elements into an HTML Collection: https://www.w3schools.com/jsref/dom_obj_htmlcollection.asp  
 
   // @TODO-3.5: MIX & MATCH, You will need the following pieces of code to build that line:
   // squares
@@ -96,14 +138,19 @@ const resetBoard = () => {
   // =
   // document
   // const
-       const squares = document.getElementsByTagName("TD")
+  const squares = document.getElementsByTagName("TD")
   // loops over the HTML Collection of TDs and clears out the Xs and Os
-  for (i=0; i < squares.length; i++) {
+  for (i = 0; i < squares.length; i++) {
 
     // will log out the id of each square as it loops over them.
     console.log(squares[i].id)
 
     // sets the innerHTML to null to replace the "X" or "O"
     squares[i].innerHTML = null
-  }  
+  }
+  board = [
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', '']
+  ]
 }
